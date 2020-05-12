792ad51e69e899fa0fd2c4e61d3e1485d4e58b33
Daniel Cruz <d.afc98@gmail.com>
2020-05-12 18:00:16 -0300

    add create product page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const routes = express.Router();
+const productController = require('./app/controllers/productController');
 
 //Instructors Routes
 routes.get('/', (req, res) => {
     return res.render("layout.njk")
 });
+
+routes.get('/products/create', productController.create);
+
+routes.get('/ads/create', (req, res) => {
+    return res.redirect('/products/create')
+});
+
+
+
 
 
 
