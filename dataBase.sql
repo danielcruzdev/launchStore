1c2886f85726cae37652ea6bde77ef1bd772651a
Daniel Cruz <d.afc98@gmail.com>
2020-07-21 18:12:59 -0300

    Create table user to implement login/logout

--- dataBase.sql
+++ dataBase.sql
@@ -24,6 +24,42 @@ CREATE TABLE "files" (
   "product_id" int
 );
 
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpg_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT 'now()',
+  "updated_at" timestamp DEFAULT 'now()'
+);
+-- Foreign KEYS
+
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+--CREATE PROCEDURE 
+CREATE FUNCTION trigger_set_timestamp()
+RETURN TRIGGER AS $$
+	BEGIN
+  	NEW.updated.at = NOW()
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql
+
+--CREATE TRIGGERS -- AUTO UPDATE PRODUCTS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+--CREATE TRIGGERS -- AUTO UPDATE USERS
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
