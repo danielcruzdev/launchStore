b90475bf20226852b25f70909111d86b91f10815
Daniel Cruz <d.afc98@gmail.com>
2020-07-28 22:05:44 -0300

    Criado recuperação de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto") 
+const User = require("../models/User")
 
 module.exports = {
     loginForm(req, res) {
@@ -26,5 +28,27 @@ module.exports = {
         } catch (error) {
             throw new Error(error)
         }
+    },
+    forgotForm(req, res){
+        try {
+            return res.render("session/forgot-password")
+
+        } catch (error) {
+            throw new Error(error)
+        }
+    },
+    async forgot(req, res){
+        const user = req.user
+
+        const token = crypto.randomBytes(20).toString("hex")
+
+        let expireDate = new Date();
+
+        expireDate = expireDate.setHours(expireDate.getHours() + 1)
+
+        await User.Update(user.Id, {
+            reset_token: token,
+            reset_token_expires: expireDate
+        })
     }
 }
--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -2,29 +2,53 @@ const User = require("../models/User");
 const { compare } = require("bcryptjs")
 
 async function login(req, res, next) {
-    const { email, password } = req.body;
-    const user = await User.FindOne({ where: { email } });
+    try {
+        const { email, password } = req.body;
+        const user = await User.FindOne({ where: { email } });
+    
+        if (!user) return res.render("session/login", {
+            user: req.body,
+            error: "Usuário não cadastrado!"
+        });
+    
+        const passed = await compare(password, user.password);
+    
+        if(!passed) return res.render("session/login", {
+            user: req.body,
+            error: "Senha incorreta!"
+        });
+    
+        req.user = user
+    
+        next();
+    } catch (error) {
+        throw new Error(error)
+    }
 
-    if (!user) return res.render("session/login", {
-        user: req.body,
-        error: "Usuário não cadastrado!"
-    });
 
-    const passed = await compare(password, user.password);
-    console.log(`PASSED? : ${passed} \n Password: ${password} \n User Password: ${user.password}`)
+}
 
-    if(!passed) return res.render("session/login", {
-        user: req.body,
-        error: "Senha incorreta!"
-    });
+async function forgot(req, res, next){
+    try {
+        const { email } = req.body
 
-    req.user = user
+        const user = await User.FindOne({ where: { email } });
+    
+        if (!user) return res.render("session/forgot-password", {
+            user: req.body,
+            error: "Email não cadastrado!"
+        });
 
-    next();
+        req.user = user
 
+        
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
 
 module.exports = {
-    login
+    login,
+    forgot
 }
